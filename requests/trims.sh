352636c45dcf351793853f4e3b93b595da1ecefb
Forest Gregg <fgregg@uchicago.edu>
2014-12-16 13:54:33 -0600

    makefiles and homicide data

--- requests/trims.sh
+++
@@ -1,7 +0,0 @@
-wget -O trim_requests.csv https://data.cityofchicago.org/api/views/uxic-zsuj/rows.csv?accessType=DOWNLOAD
-
-ogr2ogr -f "PostgreSQL" -lco GEOMETRY_NAME=geom -lco FID=gid PG:"host=localhost dbname=fgregg password=PASSWORD" trim_requests.vrt -nln trim_requests
-
-ALTER TABLE requests ALTER COLUMN "completion date" TYPE DATE USING to_date(NULLIF("completion date", ''), 'MM/DD/YYYY');
-ALTER TABLE requests ALTER COLUMN "creation date" TYPE DATE USING to_date(NULLIF("creation date", ''), 'MM/DD/YYYY');
-
